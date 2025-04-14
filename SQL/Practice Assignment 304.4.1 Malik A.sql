18e16a1f75a5c4db960fb1e432014447a9c032d8
Alston <malik.alston@accenture.com>
2025-04-14 17:13:39 -0400

    4.14.25

--- SQL/Practice Assignment 304.4.1 Malik A.sql
+++ SQL/Practice Assignment 304.4.1 Malik A.sql
@@ -1,7 +1,7 @@
 --Task 1;
 select productName as 'Name' , productLine as 'Product Line', buyPrice as 'Buy Price'
-from products
-order by buyPrice desc;
+from products --selected table;
+order by buyPrice desc; --used to sort;
 
 --Task 2;
 select contactFirstName as 'First Name', contactLastName as 'Last Name', city as 'City'
@@ -11,7 +11,23 @@ order by contactLastName asc;
 --Task 3;
 select status
 from orders
-group by status
+group by status 
 order by status asc
 
 --Task 4;
+select * --everything selected;
+from payments
+where paymentDate >= '2005-01-01' --shows paymentdates after this period of time listed;
+order by paymentDate; --paymentdate sorted ascended
+
+--Task 5;
+select lastName, firstName, email, jobTitle --things viewed from table
+from employees --employees table selected;
+where officeCode = '1' --office code 1 equals to san fran;
+order by lastName; --default sorted asceneded
+
+--Task 6;
+select productName, productLine, productScale, productVendor --things viewed from table
+from products --selected table;
+where productLine in ('Vintage Cars', 'Classic Cars'); --in used to select multi values;
+order by productLine asc; --productline sorted in ascended order
